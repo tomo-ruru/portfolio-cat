542d0a58bc72ba2e664aada2315305a94f5573a9
tomo-ruru <66007695+tomo-ruru@users.noreply.github.com>
2020-06-09 01:12:08 +0900

    first comit

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function(){
+  $(window).on('load scroll', function (){
+
+    var box = $('.fadein');
+    var animated = 'animated';
+    
+    box.each(function(){
+    
+      var boxOffset = $(this).offset().top;
+      var scrollPos = $(window).scrollTop();
+      var wh = $(window).height();
+  
+      //画面内のどの位置で処理を実行するかで「100」の値を変更
+      if(scrollPos > boxOffset - wh + 100 ){
+        $(this).addClass(animated);
+      }
+    });
+  
+  });
+});
